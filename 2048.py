fed7cc592d952d4a728778c525a7459aee9d6cf3
Yohann Front-Reignier <113431959+uvsq22200054@users.noreply.github.com>
2023-03-19 13:36:53 +0100

    Ajout de 3 fonctions + Interface graphique
    Ajout des fonctions rmd_coord, rmd_block et assign_block pour l'ajout aléatoire d'un block (il suffira de les utiliser à chaque fois que l'utilisateur bouge vers un côté)
    Modification de la grille (renomée grid) pour la créer en utilisant numpy
    Création de la fenêtre nommée 2048

--- 2048.py
+++ 2048.py
@@ -1,6 +1,33 @@
 import tkinter as tk
 import numpy as np
+from random import randint
 
-grille = [[0 for i in range(4)]]*4
-print(grille)
+######################### Création de la grille #########################
 
+grid = np.zeros((4,4), dtype = np.uint)
+
+def rdm_coord() -> tuple: 
+    '''Choisis des coordonnées aléatoire'''
+    x,y = randint(0,3), randint(0,3)
+    return(x,y)
+
+def rdm_block():
+    '''Choisis un block aléatoire'''
+    choix = [2,2,2,2,2,2,2,2,2,4] # 2 est 9 fois plus probable que 4
+    return(choix[randint(0,1)])
+
+def assign_block():
+    '''Met le bloc choisis aléatoirement aux coordonnées aléatoire'''
+    grid[rdm_coord()] = rdm_block()
+
+
+
+
+########################## Interface graphique ##########################
+root = tk.Tk()
+root.title("2048")
+
+
+print(grid)
+
+root.mainloop()
