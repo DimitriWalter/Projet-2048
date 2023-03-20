a33c5b62547537111634926dbb398ea12326f6c7
Yohann Front-Reignier <113431959+uvsq22200054@users.noreply.github.com>
2023-03-20 21:28:59 +0100

    Création de la grille pour le 2048
    Création de la fonction "UI_grid" pour afficher la grille de jeu de 2048 dans la fenêtre
    Création de la fonction "new_grid" pour créer une nouvelle matrice d'élément vide

--- 2048.py
+++ 2048.py
@@ -4,7 +4,9 @@ from random import randint
 
 ######################### Création de la grille #########################
 
-grid = np.zeros((4,4), dtype = np.uint)
+def new_grid() -> list:
+    '''Crée une nouvelle grille vide'''
+    return(np.zeros((4,4), dtype =  int))
 
 def rdm_coord() -> tuple: 
     '''Choisis des coordonnées aléatoire'''
@@ -18,16 +20,29 @@ def rdm_block():
 
 def assign_block():
     '''Met le bloc choisis aléatoirement aux coordonnées aléatoire'''
-    grid[rdm_coord()] = rdm_block()
+    new_grid()[rdm_coord()] = rdm_block()
 
+print(new_grid())
 
-
+print(new_grid().shape)
 
 ########################## Interface graphique ##########################
 root = tk.Tk()
 root.title("2048")
 
+def UI_grid():
+    SIZE = new_grid().shape[0] # Taille de la matrice
+    SIDE = 500
 
-print(grid)
+    back = tk.Canvas(background = "grey", height = SIDE, width = SIDE)
+    back.grid(row = 0, column = 0, rowspan = SIZE, columnspan = SIZE)
+    for x in range(SIZE):
+        for y in range(SIZE):
+            box = tk.Frame(background="ivory", height = SIDE//4, width = SIDE//4)
+            box.grid(row=y, column=x)
+
+
+
+UI_grid()
 
 root.mainloop()
