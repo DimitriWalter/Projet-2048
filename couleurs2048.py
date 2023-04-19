0bef97f87a52e323694b3181a1dce19d15aa36d2
Dimitri WALTER <dimitri.walter@ens.uvsq.fr>
2023-04-19 22:15:17 +0200

    réajustement

---
+++ couleurs2048.py
@@ -0,0 +1,41 @@
+FONTS = {
+	2: ("Helvetica", 50, "bold"),
+	4: ("Helvetica", 50, "bold"),
+	8: ("Helvetica", 50, "bold"),
+	16: ("Helvetica", 50, "bold"),
+	32: ("Helvetica", 45, "bold"),
+	64: ("Helvetica", 45, "bold"),
+	128: ("Helvetica", 45, "bold"),
+	256: ("Helvetica", 45, "bold"),
+	512: ("Helvetica", 45, "bold"),
+	1024: ("Helvetica", 45, "bold"),
+	2048: ("Helvetica", 45, "bold"),
+}
+
+COULEURS_CASES = {
+	2: "#fcefe6",
+	4: "#f2e8cb",
+	8: "#f5b682",
+	16: "#f29446",
+	32: "#ff775c",
+	64: "#e64c2e",
+	128: "#ede291",
+	256: "#fce130",
+	512: "#ffdb4a",
+	1024: "#f0b922",
+	2048: "#fad74d"
+}
+
+COULEURS_NOMBRES = {
+	2: "#695c57",
+	4: "#695c57",
+	8: "#ffffff",
+	16: "#ffffff",
+	32: "#ffffff",
+	64: "#ffffff",
+	128: "#ffffff",
+	256: "#ffffff",
+	512: "#ffffff",
+	1024: "#ffffff",
+	2048: "#ffffff"
+}
